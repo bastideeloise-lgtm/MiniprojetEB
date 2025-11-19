4482f8e244e6ea9ce8ae4a5e6a4878b130c8c303
bastideeloise-lgtm <bastideeloise@gmail.com>
2025-11-19 09:24:04 +0100

    Add files via upload

---
+++ MP1_ELOISE_BASTIDE.js
@@ -0,0 +1,4 @@
+let texte = document.getElementById("chaineFusion").innerHTML;
+let autreTexte = " par comprendre cette exo!";
+let fusion = texte + autreTexte;
+document.getElementById("chaineFusion").innerHTML = fusion;
